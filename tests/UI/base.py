fedea7c8837a8926f3e5512b17797234b33ce39e
wolf637 <alexey.volkov@live.com>
2018-07-23 21:40:21 -0700

    Add options to webdriver settings to resolve problems of failing to start on server

--- tests/UI/base.py
+++ tests/UI/base.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from tests.UI.page_objects.UI_map import BASE_URL
 
 WAIT_TIME = 10
@@ -9,6 +10,12 @@ class TestClassBase(object):
     def setup_class(cls):
         "Runs once per class"
         # TODO Change path on server
+
+
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         # cls.driver = webdriver.Chrome('/Users/avolkov/Documents/05_Development/PMA/property_manager/tests/UI/drivers/mac/chromedriver')
         cls.driver = webdriver.Chrome('/home/pmadmin/property-manager-project/property-manager/tests/UI/drivers/linux/chromedriver')
         cls.driver.get(BASE_URL)
