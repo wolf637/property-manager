ba981e9919fe0228fe79c46be8f1910dd4a8736f
wolf637 <alexey.volkov@live.com>
2018-11-05 12:04:23 -0800

    Move model registrations into corresponding apps

--- guests/admin.py
+++ guests/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-
+from guests.models import Guest
 
 # Register your models here.
+admin.site.register(Guest)
--- room_types/admin.py
+++ room_types/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from room_types.models import RoomType, Bed
+# Register your models here.
 
 # Register your models here.
+admin.site.register(RoomType)
+admin.site.register(Bed)
