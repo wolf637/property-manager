f09c41906b13641dc57f8571571554b93af380c5
wolf637 <alexey.volkov@live.com>
2018-11-05 11:35:10 -0800

    Change blank attribute on Room model to False

--- rooms/models.py
+++ rooms/models.py
@@ -9,7 +9,7 @@ class Room(models.Model):
     name = models.TextField(max_length=80)
     property = models.ForeignKey(Property, on_delete=models.DO_NOTHING, blank=True)
     description = models.TextField(max_length=1000, default="")
-    room_type = models.ForeignKey(RoomType, on_delete=models.DO_NOTHING, blank=True)
+    room_type = models.ForeignKey(RoomType, on_delete=models.DO_NOTHING, blank=False)
     empty = models.BooleanField(default=True)
     ready = models.BooleanField(default=True)
 
